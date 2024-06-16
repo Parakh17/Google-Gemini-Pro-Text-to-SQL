e3c3cdd06367dea7ced9b1c688debe846c69d693
Parakh Singhal <parakhsinghal2000@gmail.com>
2024-06-16 18:53:47 +0530

    sql.py

---
+++ sql.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+connection = sqlite3.connect("student.db")
+
+
+cursor=connection.cursor()
+
+table_info = """
+Create table if not exists Student ( name varchar(25), class varchar(25), section varchar(25), marks int);
+"""
+
+
+cursor.execute(table_info)
+
+cursor.execute("""INsert into student values("hritihk", "9", "b", 40)""")
+cursor.execute("""insert into student values("parakh","12","a",90 )""")
+cursor.execute("""Insert into student values("chavi","11","b",99)""")
+cursor.execute("""insert into student values("shraddha", "5","d",96)""")
+cursor.execute("""Insert into student values("yash","4","a",99)""")
+cursor.execute("""insert into student values("harsh","8","f",44)""")
+
+data=cursor.execute("""select * from student""")
+
+for r in data:
+    print(r)
+#print(data) we cannot print this
+
+connection.commit()
+connection.close()
+
+
+
